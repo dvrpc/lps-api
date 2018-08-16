67907a573afdb31aa42d6bb90803d1db89bb4364
Robert Beatty <beattyre1@gmail.com>
2018-08-16 12:49:28 -0400

    Added lookup for station inputs. @TODO: populate year input based on selected station

---
+++ data/stationslookup.js
@@ -0,0 +1,1085 @@
+const stations = [
+    {
+        "station": "46th Street",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "60th Street",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "69th Street Transportation Center",
+        "years": [
+            2011,
+            2017
+        ]
+    },
+    {
+        "station": "80th St & Eastwick Ave Loop",
+        "years": [
+            2015
+        ]
+    },
+    {
+        "station": "9th Street",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Ambler",
+        "years": [
+            2002,
+            2009,
+            2017
+        ]
+    },
+    {
+        "station": "Angora",
+        "years": [
+            2003
+        ]
+    },
+    {
+        "station": "Ardmore",
+        "years": [
+            2002,
+            2009,
+            2017
+        ]
+    },
+    {
+        "station": "Ardmore Avenue",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Ardmore Junction",
+        "years": [
+            2009,
+            2016
+        ]
+    },
+    {
+        "station": "Ardsley",
+        "years": [
+            2000,
+            2006
+        ]
+    },
+    {
+        "station": "AT&T",
+        "years": [
+            2018
+        ]
+    },
+    {
+        "station": "Atco",
+        "years": [
+            2002,
+            2006
+        ]
+    },
+    {
+        "station": "Bala",
+        "years": [
+            2005,
+            2017
+        ]
+    },
+    {
+        "station": "Beechwood-Brookline",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Berwyn",
+        "years": [
+            2005,
+            2011,
+            2017
+        ]
+    },
+    {
+        "station": "Bethayres",
+        "years": [
+            2005,
+            2010
+        ]
+    },
+    {
+        "station": "Bordentown",
+        "years": [
+            2008
+        ]
+    },
+    {
+        "station": "Bridgeport",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Bristol",
+        "years": [
+            2001,
+            2013
+        ]
+    },
+    {
+        "station": "Bryn Mawr",
+        "years": [
+            2009,
+            2017
+        ]
+    },
+    {
+        "station": "Burlington South",
+        "years": [
+            2008
+        ]
+    },
+    {
+        "station": "Carpenter Lane",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Chalfont",
+        "years": [
+            2004,
+            2011,
+            2014,
+            2017
+        ]
+    },
+    {
+        "station": "Chelten Avenue",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Cheltenham",
+        "years": [
+            2010
+        ]
+    },
+    {
+        "station": "Cherry Hill",
+        "years": [
+            2002,
+            2006
+        ]
+    },
+    {
+        "station": "Chester Transportation Center",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Chestnut Hill East",
+        "years": [
+            2016
+        ]
+    },
+    {
+        "station": "Chestnut Hill West",
+        "years": [
+            2016
+        ]
+    },
+    {
+        "station": "Cinnaminson",
+        "years": [
+            2008
+        ]
+    },
+    {
+        "station": "Clifton-Aldan",
+        "years": [
+            2009,
+            2017
+        ]
+    },
+    {
+        "station": "Colmar",
+        "years": [
+            2004,
+            2011,
+            2014
+        ]
+    },
+    {
+        "station": "Conshohocken",
+        "years": [
+            2006,
+            2014
+        ]
+    },
+    {
+        "station": "Cornwells Heights",
+        "years": [
+            2001,
+            2013,
+            2017
+        ]
+    },
+    {
+        "station": "Crestmont",
+        "years": [
+            2006
+        ]
+    },
+    {
+        "station": "Croydon",
+        "years": [
+            2001,
+            2013,
+            2017
+        ]
+    },
+    {
+        "station": "Cynwyd",
+        "years": [
+            2005,
+            2017
+        ]
+    },
+    {
+        "station": "Daylesford",
+        "years": [
+            2005,
+            2011,
+            2017
+        ]
+    },
+    {
+        "station": "Dekalb Street",
+        "years": [
+            2011
+        ]
+    },
+    {
+        "station": "Delaware Valley College",
+        "years": [
+            2003,
+            2014
+        ]
+    },
+    {
+        "station": "Devon",
+        "years": [
+            2011,
+            2017
+        ]
+    },
+    {
+        "station": "Downingtown",
+        "years": [
+            2005,
+            2011,
+            2017
+        ]
+    },
+    {
+        "station": "Doylestown",
+        "years": [
+            2002,
+            2014,
+            2017
+        ]
+    },
+    {
+        "station": "Drexeline",
+        "years": [
+            2015
+        ]
+    },
+    {
+        "station": "East Falls",
+        "years": [
+            2014
+        ]
+    },
+    {
+        "station": "Eastwick",
+        "years": [
+            2003,
+            2015
+        ]
+    },
+    {
+        "station": "Elkins Park",
+        "years": [
+            2000,
+            2015
+        ]
+    },
+    {
+        "station": "Elm Street",
+        "years": [
+            2012
+        ]
+    },
+    {
+        "station": "Elwyn",
+        "years": [
+            2002,
+            2009,
+            2015
+        ]
+    },
+    {
+        "station": "Exton",
+        "years": [
+            2002,
+            2005,
+            2011,
+            2016
+        ]
+    },
+    {
+        "station": "Fern Rock",
+        "years": [
+            2002,
+            2010
+        ]
+    },
+    {
+        "station": "Ferry Avenue",
+        "years": [
+            2002,
+            2006
+        ]
+    },
+    {
+        "station": "Florence",
+        "years": [
+            2008
+        ]
+    },
+    {
+        "station": "Forest Hills",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Fort Washington",
+        "years": [
+            2002,
+            2012
+        ]
+    },
+    {
+        "station": "Fortuna",
+        "years": [
+            2003,
+            2012
+        ]
+    },
+    {
+        "station": "Fox Chase",
+        "years": [
+            2002,
+            2010
+        ]
+    },
+    {
+        "station": "Frankford Transportation Center",
+        "years": [
+            2013
+        ]
+    },
+    {
+        "station": "Frazer Park and Ride (US30 at US202 Interchange, at northbound on-ramp)",
+        "years": [
+            2011
+        ]
+    },
+    {
+        "station": "Germantown",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Glenolden",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Glenside",
+        "years": [
+            2000,
+            2015
+        ]
+    },
+    {
+        "station": "Gravers Lane",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Gulph Mills",
+        "years": [
+            2011
+        ]
+    },
+    {
+        "station": "Gwynedd Valley",
+        "years": [
+            2002,
+            2012,
+            2017
+        ]
+    },
+    {
+        "station": "Hamilton",
+        "years": [
+            2002,
+            2008
+        ]
+    },
+    {
+        "station": "Hatboro",
+        "years": [
+            2004,
+            2009,
+            2017
+        ]
+    },
+    {
+        "station": "Haverford",
+        "years": [
+            2009,
+            2016
+        ]
+    },
+    {
+        "station": "Highland",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Highland Avenue",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Holmesburg Junction",
+        "years": [
+            2005
+        ]
+    },
+    {
+        "station": "Hughes Park",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Ivy Ridge",
+        "years": [
+            2014
+        ]
+    },
+    {
+        "station": "Jenkintown-Wyncote",
+        "years": [
+            2000,
+            2015
+        ]
+    },
+    {
+        "station": "Lamokin Street",
+        "years": [
+            2003
+        ]
+    },
+    {
+        "station": "Langhorne",
+        "years": [
+            2005,
+            2010,
+            2017
+        ]
+    },
+    {
+        "station": "Lansdale",
+        "years": [
+            2002,
+            2012,
+            2017
+        ]
+    },
+    {
+        "station": "Lansdowne",
+        "years": [
+            2016
+        ]
+    },
+    {
+        "station": "Levittown",
+        "years": [
+            2005
+        ]
+    },
+    {
+        "station": "Lindenwold",
+        "years": [
+            2002,
+            2006
+        ]
+    },
+    {
+        "station": "Link Belt",
+        "years": [
+            2003
+        ]
+    },
+    {
+        "station": "Lionville Park and Ride (PA 113 east of PA 100)",
+        "years": [
+            2011
+        ]
+    },
+    {
+        "station": "Main Street",
+        "years": [
+            2012
+        ]
+    },
+    {
+        "station": "Malvern",
+        "years": [
+            2005,
+            2011,
+            2017
+        ]
+    },
+    {
+        "station": "Malvern Park and Ride (Matthews Road at PA 29/US 202 Interchange)",
+        "years": [
+            2011
+        ]
+    },
+    {
+        "station": "Marcus Hook",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Matsonford",
+        "years": [
+            2011
+        ]
+    },
+    {
+        "station": "Meadowbrook",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Media",
+        "years": [
+            2002,
+            2009,
+            2015
+        ]
+    },
+    {
+        "station": "Melrose Park",
+        "years": [
+            2015
+        ]
+    },
+    {
+        "station": "Merion",
+        "years": [
+            2005,
+            2009
+        ]
+    },
+    {
+        "station": "Miquon",
+        "years": [
+            2013
+        ]
+    },
+    {
+        "station": "Morton",
+        "years": [
+            2009,
+            2017
+        ]
+    },
+    {
+        "station": "Moylan-Rose Valley",
+        "years": [
+            2016
+        ]
+    },
+    {
+        "station": "Narberth",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Neshaminy Falls",
+        "years": [
+            2005,
+            2010,
+            2017
+        ]
+    },
+    {
+        "station": "New Britain",
+        "years": [
+            2003,
+            2014
+        ]
+    },
+    {
+        "station": "Noble",
+        "years": [
+            2000,
+            2009,
+            2017
+        ]
+    },
+    {
+        "station": "Norristown Transportation Center",
+        "years": [
+            2011
+        ]
+    },
+    {
+        "station": "North Hills",
+        "years": [
+            2000,
+            2012
+        ]
+    },
+    {
+        "station": "North Wales",
+        "years": [
+            2002,
+            2012
+        ]
+    },
+    {
+        "station": "Norwood",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Olney",
+        "years": [
+            2010
+        ]
+    },
+    {
+        "station": "Oreland",
+        "years": [
+            2002,
+            2012
+        ]
+    },
+    {
+        "station": "Overbrook",
+        "years": [
+            2005,
+            2009
+        ]
+    },
+    {
+        "station": "Paoli",
+        "years": [
+            2005,
+            2006,
+            2008,
+            2011,
+            2017
+        ]
+    },
+    {
+        "station": "Parkesburg",
+        "years": [
+            2005,
+            2011,
+            2016,
+            2017
+        ]
+    },
+    {
+        "station": "Pattison",
+        "years": [
+            2003
+        ]
+    },
+    {
+        "station": "Penfield",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Penllyn",
+        "years": [
+            2002
+        ]
+    },
+    {
+        "station": "Pennbrook",
+        "years": [
+            2004,
+            2011
+        ]
+    },
+    {
+        "station": "Philmont",
+        "years": [
+            2009,
+            2017
+        ]
+    },
+    {
+        "station": "Primos",
+        "years": [
+            2013
+        ]
+    },
+    {
+        "station": "Princeton Junction",
+        "years": [
+            2002
+        ]
+    },
+    {
+        "station": "Prospect Park",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Queen Lane",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Radnor",
+        "years": [
+            2009,
+            2016,
+            2017
+        ]
+    },
+    {
+        "station": "Roebling",
+        "years": [
+            2008
+        ]
+    },
+    {
+        "station": "Rosemont",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Roslyn",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Route 73/Pennsauken",
+        "years": [
+            2008
+        ]
+    },
+    {
+        "station": "Rydal",
+        "years": [
+            2006,
+            2010
+        ]
+    },
+    {
+        "station": "Ryers",
+        "years": [
+            2005,
+            2010
+        ]
+    },
+    {
+        "station": "Scenic Road",
+        "years": [
+            2015
+        ]
+    },
+    {
+        "station": "Secane",
+        "years": [
+            2006
+        ]
+    },
+    {
+        "station": "Sedgwick",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Somerton",
+        "years": [
+            2005,
+            2010
+        ]
+    },
+    {
+        "station": "Spring Mill",
+        "years": [
+            2014
+        ]
+    },
+    {
+        "station": "Springfield Road",
+        "years": [
+            2015
+        ]
+    },
+    {
+        "station": "St. Davids",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "St. Martins",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Stenton",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Strafford",
+        "years": [
+            2009,
+            2011,
+            2017
+        ]
+    },
+    {
+        "station": "Swarthmore",
+        "years": [
+            2016
+        ]
+    },
+    {
+        "station": "Thorndale",
+        "years": [
+            2002,
+            2005,
+            2011,
+            2016,
+            2017
+        ]
+    },
+    {
+        "station": "Torresdale",
+        "years": [
+            2005,
+            2017
+        ]
+    },
+    {
+        "station": "Township Line Road",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Trenton",
+        "years": [
+            2008
+        ]
+    },
+    {
+        "station": "Trevose",
+        "years": [
+            2005,
+            2010,
+            2017
+        ]
+    },
+    {
+        "station": "Tulpehocken",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Upsal",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "US 1 & PA 272 Park and Ride",
+        "years": [
+            2016
+        ]
+    },
+    {
+        "station": "US 1 & PA 472 Park and Ride",
+        "years": [
+            2016
+        ]
+    },
+    {
+        "station": "Villanova",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Warminster",
+        "years": [
+            2002,
+            2017
+        ]
+    },
+    {
+        "station": "Washington Lane",
+        "years": [
+            2017
+        ]
+    },
+    {
+        "station": "Wayne",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "West Goshen Park and Ride (Paoli Pike at US 202 Interchange, adjacent to West Goshen Shopping Ctr)",
+        "years": [
+            2011
+        ]
+    },
+    {
+        "station": "West Trenton",
+        "years": [
+            2002,
+            2009
+        ]
+    },
+    {
+        "station": "Whitford",
+        "years": [
+            2005,
+            2011,
+            2017
+        ]
+    },
+    {
+        "station": "Willow Grove",
+        "years": [
+            2009,
+            2017
+        ]
+    },
+    {
+        "station": "Wissahickon",
+        "years": [
+            2003,
+            2014
+        ]
+    },
+    {
+        "station": "Wissinoming",
+        "years": [
+            2003
+        ]
+    },
+    {
+        "station": "Woodbourne",
+        "years": [
+            2005,
+            2010,
+            2017
+        ]
+    },
+    {
+        "station": "Woodcrest",
+        "years": [
+            2002,
+            2006
+        ]
+    },
+    {
+        "station": "Woodland Avenue (Springfield)",
+        "years": [
+            2015
+        ]
+    },
+    {
+        "station": "Wyndmoor",
+        "years": [
+            2016
+        ]
+    },
+    {
+        "station": "Wynnefield Avenue",
+        "years": [
+            2005,
+            2017
+        ]
+    },
+    {
+        "station": "Wynnewood",
+        "years": [
+            2009,
+            2016
+        ]
+    },
+    {
+        "station": "Wynnewood Road",
+        "years": [
+            2009
+        ]
+    },
+    {
+        "station": "Yardley",
+        "years": [
+            2009
+        ]
+    }
+]
+
+export {stations}
