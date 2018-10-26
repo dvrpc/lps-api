1d1f52d96d02bad39c590117f03dab0261713c66
Robert Beatty <beattyre1@gmail.com>
2018-10-26 16:37:55 -0400

    Closes #6, #8, #9. Did some stuff but I'm tired and don't want to write a useful commit message

--- src/utils/get-passenger-rail-layers.js
+++ src/utils/get-passenger-rail-layers.js
@@ -1,10 +1,9 @@
 export const getRailLayer = (lineName, lineColor) => {
-    console.log({lineName})
     return {
         id: 'railLayer',
         type: 'line',
         source: 'passengerRailLines',
-        filter: ['match', ['get', 'LINE_NAME'], lineName, true, false],
+        // filter: ['match', ['get', 'LINE_NAME'], lineName, true, false],
         paint: {
         "line-color": lineColor,
         "line-width":['interpolate', ['linear'], ['zoom'], 8, 1, 12, 5],
@@ -12,4 +11,15 @@ export const getRailLayer = (lineName, lineColor) => {
         "line-dasharray": [2,2]
         }
     }
+}
+
+export const loadLayers = (map, layers) =>{
+    for (let source in layers){
+        map.addSource(source, layers[source].sourceDef)
+        for (let layer in layers[source].layers){
+            map.addLayer(layers[source].layers[layer], 'waterway-label')
+        }
+        
+    }
+
 }
