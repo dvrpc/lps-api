d99f3bae6c06550ca916b77e2d98c77e8c514fcc
Robert Beatty <beattyre1@gmail.com>
2019-01-21 11:35:30 -0500

    Upgrade to webpack 4 & babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 var path = require('path'),
         HTMLWebpackPlugin = require('html-webpack-plugin'),
-        ExtractTextPlugin = require('extract-text-webpack-plugin'),
-        webpack = require('webpack')
+        ExtractTextPlugin = require('extract-text-webpack-plugin')
 var extractPlugin = new ExtractTextPlugin({
     filename: './bundle.styles.css'
 })
@@ -9,7 +8,8 @@ var extractPlugin = new ExtractTextPlugin({
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
-    entry: ['core-js/fn/promise', `./js/index.js`], // main JS file for application
+    entry: ['core-js/fn/promise', `./js/index.js`], // main JS file for application,
+    mode: "development",
     output: {
         path: `${__dirname}/dist`, // defining output path for build files
         filename: 'js/[name].bundle.js' // defining naming convention for bundled dist file
@@ -22,7 +22,7 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 options: {
-                    presets: ["env", "es2015", 'stage-2'] // convert everything to ES2015
+                    presets: ["@babel/preset-env"] // convert everything to ES2015
                 } 
             },
             // want to bundle your CSS files? i got u fam
