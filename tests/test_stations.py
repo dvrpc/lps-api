7ac1de0ddbd39dc8c5f3f74f385ff16e3b3d20e4
Kris Warner <kdwarn@protonmail.com>
2022-07-21 14:47:55 -0400

    Test that stations endpoint returns items

--- tests/test_stations.py
+++ tests/test_stations.py
@@ -6,3 +6,4 @@ endpoint = "api/lps/v1/stations"
 def test_success_spot_check(client):
     response = client.get(endpoint)
     assert response.status_code == 200
+    assert len(response.json()) > 0
